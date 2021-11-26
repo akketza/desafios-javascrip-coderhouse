6f13fe98aaf818c04e6212e827a7406c3b8bc726
Juan Garcia <garciaa.juan.ignacio@gmail.com>
2021-11-26 18:32:46 -0300

    desafio 3 ciclos/iteraciones

---
+++ main.js
@@ -0,0 +1,9 @@
+let numero = -1
+let acumulador = 0
+
+while (numero != 0) {
+    numero = parseInt(prompt("Ingrese un numero"))
+    acumulador = acumulador + numero
+}
+
+console.log(`El resultado es ${acumulador}`)
